785724138f6aff23b99d5e618f1e3d73dd9dd67a
Wouter Jehee <wouterjehee@gmail.com>
2020-01-23 21:33:10 +0100

    added conditionals to check hostname for scripts

--- scripts/.scripts/launch_polybar.sh
+++ scripts/.scripts/launch_polybar.sh
@@ -4,10 +4,16 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-polybar desktop-bar-1 &
-polybar desktop-bar-2 &
-
-# echo "launched polybar!"
+if [ $(hostname) = rustyLaptop ]
+then
+    polybar laptop-bar &
+    echo "Laptop"
+elif [ $(hostname) = rustyDesktop ]
+then
+    polybar desktop-bar-1 &
+    polybar desktop-bar-2 &
+    echo "Desktop"
+fi
 
 
 
