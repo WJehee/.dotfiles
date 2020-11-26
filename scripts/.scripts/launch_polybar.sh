843b4021f75c9a3b08d1938fe023c9dc7411b5ec
Wouter Jehee <wouterjehee@tutanota.com>
2020-11-26 16:56:48 +0000

    adds install script + polybar changes

--- scripts/.scripts/launch_polybar.sh
+++ scripts/.scripts/launch_polybar.sh
@@ -6,14 +6,12 @@ while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 if [ $(hostname) = rustyLaptop ]
 then
-    polybar laptop-bar &
+    polybar main-bar &
     echo "Laptop"
 elif [ $(hostname) = rustyDesktop ]
 then
-    polybar desktop-bar-1 &
-    polybar desktop-bar-2 &
+    polybar main-bar &
+    polybar desktop-2 &
     echo "Desktop"
 fi
-
-
 
