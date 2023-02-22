2b0eab265f79828331ace7cf0f9f8e91bc658774
Wouter Jehee <wouterjehee@tutanota.com>
2023-02-22 11:24:30 +0100

    add gopls for lsp

--- nvim/.config/nvim/after/plugin/language.lua
+++ nvim/.config/nvim/after/plugin/language.lua
@@ -3,7 +3,7 @@ local lsp = require("lsp-zero")
 lsp.preset("recommended")
 
 lsp.ensure_installed({
-    'rust_analyzer', 'pyright', 'html', 'hls', 'cssls', 'vimls', 'texlab', "lua_ls"
+    'rust_analyzer', 'pyright', 'html', 'cssls', 'vimls', 'texlab', 'lua_ls', 'gopls'
 })
 
 local cmp = require'cmp'
